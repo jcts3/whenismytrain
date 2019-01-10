8f1d7690bd5f89c5f5d3e03dea25b4ea118c3b69
James Stratford <36135454+jcts3@users.noreply.github.com>
2019-01-10 23:27:42 +0000

    added promise to base call function, callbacks to other functions.

--- index.js
+++ index.js
@@ -1,39 +1,62 @@
-const url = require('url');
 const request = require('request');
 const info = require('./info.json');
 
 const prefStations = info['Preferred Stations'];
-
 const prefStation = prefStations[0];
-
 const creds = info.credentials;
-
-
 const apiEP = info.Endpoint;
 
-exports.getStation = (station) => {
-  const address = `https://${apiEP}/json/search/${station.CRS}`;
-  request.get(address, (err, response, body) => {
-    if (err) {
-      console.log(err.message);
-    }
-    body = JSON.parse(body);
-    console.log(body);
-    return JSON.parse(body);
-    // Gets here too late - need to promisify!!!
-  }).auth(creds.Username, creds.Password, true);
+const getStation = (station) => {
+  return new Promise((resolve, reject) => {
+    const address = `https://${apiEP}/json/search/${station.CRS}`;
+    request.get(address, (err, response, body) => {
+      if (err) {
+        console.log(err.message);
+        reject(err);
+      }
+      // console.log(JSON.parse(body));
+      resolve(JSON.parse(body));
+      // Gets here too late - need to promisify!!!
+    }).auth(creds.Username, creds.Password, true);
+  });
 };
 
-exports.getStationInfo = station => exports.getStation(station).location;
+exports.getStationOnly = (station, cb) => {
+  const promise = getStation(station);
+  promise.then((body) => {
+    cb(body);
+  });
+};
 
-exports.getStationNextTrains = station => exports.getStation(station).services;
+exports.getStationInfo = (station, cb) => {
+  const promise = getStation(station);
+  promise.then((body) => {
+    cb(body.location);
+  });
+};
 
-exports.getStationTrainNum = (station, num) => exports.getStationNextTrains(station)[num];
+exports.getStationNextTrains = (station, num = -1, cb) => {
+  const promise = getStation(station);
+  promise.then((body) => {
+    if (num > -1) {
+      cb(body.services[num]);
+    } else {
+      cb(body.services);
+    }
+  });
+};
 
-exports.getStationNextTrain = station => exports.getStationTrainNum(station, 0);
+exports.getStationTrainNum = (station, num, cb) => exports.getStationNextTrains(station, num, cb);
+
+exports.getStationNextTrain = (station, cb) => exports.getStationNextTrains(station, 0, cb);
+
+exports.getTrainLocationDetail = train => train.locationDetail;
+// const stationPromise = (exports.getStationNextTrain(prefStation.CR));
 
 
-console.log(a);
-// module.exports = {
-//   getStation: getStation(),
-// };
+// TESTING AREA
+
+exports.getStationNextTrain(prefStation, (train) => {
+  // console.log(train);
+  console.log(exports.getTrainLocationDetail(train));
+});
