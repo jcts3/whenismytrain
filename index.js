6728a9ba8ef36b49448f2e342161edf75a748aca
James Stratford <36135454+jcts3@users.noreply.github.com>
2019-01-15 23:53:17 +0000

    simplify index.js - remove unrequired items

--- index.js
+++ index.js
@@ -2,27 +2,7 @@ const stationInfo = require('./stationInfo');
 const routeInfo = require('./routeInfo');
 const serviceInfo = require('./serviceInfo');
 
-const info = require('./info.json');
-
-const prefStations = info['Preferred Stations'];
-const prefStation = prefStations[1];
-const destStation = prefStations[2];
-const creds = info.credentials;
-const apiEP = info.Endpoint;
-
-// TESTING AREA
-
-// exports.getStationNextTrain(prefStation, (train) => {
-//   // console.log(train);
-//   console.log(exports.getTrainLocationDetail(train));
-// });
-routeInfo.getRouteNextService(prefStation, destStation, (res) => {
-  console.log(res);
-});
-
-exports = {
-  creds,
-  apiEP,
+module.exports = {
   stationInfo,
   routeInfo,
   serviceInfo,
