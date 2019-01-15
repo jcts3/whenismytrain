d99d9a1af2f7f8d355a5cf3e8d2f5c5b4089fb15
James Stratford <36135454+jcts3@users.noreply.github.com>
2019-01-15 23:12:59 +0000

    refactor date management - use moment.js functionality

--- service.js
+++ service.js
@@ -7,17 +7,23 @@ const apiEP = info.Endpoint;
 
 const faveService = info.FaveServices[1];
 
-exports.getDayObject = () => {
-  const day = {
-    year: '2019',
-    month: '01',
-    date: '15',
-  };
-  return day;
-};
+// exports.getDayObject = (date, month, year) => {
+//   if (parseInt(date, 10) < 10) date = `0${parseInt(day, 10)}`;
+//   if (parseInt(month, 10) < 10) month = `0${parseInt(month, 10)}`;
+//   const dayObj = {
+//     date,
+//     month,
+//     year,
+//   }
+//   return dayObj;
+// };
+
+exports.getTodayObject = () => exports.getDayObject(Date.now());
+
+exports.getDayObject = date => moment(date).format('YYYY/MM/DD');
 
 const getService = service => new Promise((resolve, reject) => {
-  const d = exports.getDayObject();
+  const d = exports.getDayObject(Date.now());
   const address = `https://${apiEP}/json/service/${service.SUID}/${d.year}/${d.month}/${d.date}`;
   request.get(address, (err, response, body) => {
     if (err) {
