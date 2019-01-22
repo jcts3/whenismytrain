ca59771fb927516c486c6a8f22640fb909924449
James Stratford <36135454+jcts3@users.noreply.github.com>
2019-01-22 23:13:44 +0000

    Add details + plan to demo.js

--- demo.js
+++ demo.js
@@ -9,15 +9,28 @@ const prefStations = info['Preferred Stations'];
 const prefStation = prefStations[1];
 const destStation = prefStations[2];
 
-
+// Returns basic info on next service between 2 stations
 rtt.routeInfo.getRouteNextService(prefStation, destStation, (res) => {
-  debug(res);
+  // sdebug(res);
 });
-
+// Returns detailed info on next service of particular service
 rtt.serviceInfo.getServiceAll(faveService, (res) => {
-  debug(res);
+  // debug(res);
+});
+// Returns info of next train at station
+rtt.stationInfo.getStationNextTrain(prefStation, (res) => {
+  // debug(res);
 });
 
-rtt.stationInfo.getStationNextTrain(prefStation, (res) => {
-  debug(res);
-});
+
+// when is my train
+// takes input of starterStation, enderStation, 
+// (v2) uses to find out next 5 trains.
+// (v2) works out quickest train/earliest arriver
+// (v1) finds next train
+// gets serviceId of (best) train
+// use serviceId to get details
+// key details: 
+//     DEPT TIMES: departure time, arrival time (dept stat), delay?, delay length,
+//     ARRIV TIMES: arrival time (dest stat), delay?, delay length
+//     ROUTE INFO: dept platform #, origin, final destination, dest platfrom #
