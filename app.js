736e3306ebd453bcf1d928776de3b4ab215238f6
James Stratford <36135454+jcts3@users.noreply.github.com>
2019-01-22 23:40:03 +0000

    Addition of express app to return information.

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+
+const rtt = require('./index');
+const info = require('./info.json');
+
+const port = 8081;
+const app = express();
+const serviceRouter = express.Router();
+
+serviceRouter.get('/:dept/:dest/', (req, res, next) => {
+  const prefStation = req.params.dept;
+  const destStation = req.params.dest;
+  rtt.routeInfo.getRouteNextService(prefStation, destStation, (service) => {
+    rtt.serviceInfo.getServiceAll(service, (serviceInfo) => {
+      res.json(serviceInfo);
+    });
+  });
+});
+
+
+app.use(serviceRouter);
+
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)}`);
+});
--- routeInfo.js
+++ routeInfo.js
@@ -7,7 +7,8 @@ const creds = info.credentials;
 
 /* ROUTES */
 const getRoute = (origin, destination) => new Promise((resolve, reject) => {
-  const address = `https://${apiEP}json/search/${origin.CRS}/to/${destination.CRS}`;
+  // const address = `https://${apiEP}json/search/${origin.CRS}/to/${destination.CRS}`;
+  const address = `https://${apiEP}json/search/${origin}/to/${destination}`;
   request.get(address, (err, response, body) => {
     if (err) {
       debug(err.message);
@@ -33,7 +34,7 @@ exports.getRouteNextServices = (origin, destination, num = -1, cb) => {
     }
   });
 };
-exports.getRouteNumService = (origin, destination, num, cb) => exports.getRouteNextServices(origin, destination, num, cb);
+// exports.getRouteNumService = (origin, destination, num, cb) => exports.getRouteNextServices(origin, destination, num, cb);
 
 exports.getRouteNextService = (origin, destination, cb) => {
   exports.getRouteNextServices(origin, destination, 0, cb);
