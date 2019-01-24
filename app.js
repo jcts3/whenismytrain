648bfc5240525d5476596f504e09ec6f65b8bcfa
James Stratford <36135454+jcts3@users.noreply.github.com>
2019-01-24 23:08:05 +0000

    broken departure info route added

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ const port = 8081;
 const app = express();
 const serviceRouter = express.Router();
 
+serviceRouter.get('/:dept/:dest/departureinfo', (req, res, next) => {
+  const origStation = req.params.dept;
+  const destStation = req.params.dest;
+  rtt.routeInfo.getRouteNextService(origStation, destStation, (service) => {
+    rtt.serviceInfo.getServiceAll(service, (serviceInfo) => {
+      // res.json(serviceInfo);
+      const resp = {};
+      resp.trainArrivesAtDept = (serviceInfo.locations).prototype.filter(element => element.crs === origStation);
+      res.json(resp);
+    });
+  });
+});
+
 serviceRouter.get('/:dept/:dest/', (req, res, next) => {
   const prefStation = req.params.dept;
   const destStation = req.params.dest;
@@ -25,3 +38,9 @@ app.use(serviceRouter);
 app.listen(port, () => {
   debug(`listening on port ${chalk.green(port)}`);
 });
+
+
+// key details: 
+//     DEPT TIMES: departure time, arrival time (dept stat), delay?, delay length,
+//     ARRIV TIMES: arrival time (dest stat), delay?, delay length
+//     ROUTE INFO: dept platform #, origin, final destination, dest platfrom #
